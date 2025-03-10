786a9e882406a7ffda2af0b54a9216d4acc31087
h70076222 <142376079+h70076222@users.noreply.github.com>
2025-03-11 00:32:45 +0800

    Update wireguard.sh

--- trunk/user/wireguard/wireguard.sh
+++ trunk/user/wireguard/wireguard.sh
@@ -41,3 +41,107 @@ ifconfig hxsdwan arp
 else
 logger -t "异地组网" "启动失败"
 fi
+
+wireguard_error="错误：${WIREGUARD} 未运行，请运行成功后执行此操作！"
+wireguard_process=$(pidof vnp)
+wireguardpath=$(dirname "$WIREGUARD")
+cmdfile="/tmp/vpn_cmd.log"
+
+vpn_info() {
+	if [ ! -z "$wireguard_process" ] ; then
+		cd $wireguardpath
+		/usr/bin/vpn --info >$cmdfile 2>&1
+	else
+		echo "$wireguard_error" >$cmdfile 2>&1
+	fi
+	exit 1
+}
+
+vpn_all() {
+	if [ ! -z "$wireguard_process" ] ; then
+		cd $wireguardpath
+		/usr/bin/vpn --all >$cmdfile 2>&1
+	else
+		echo "$wireguard_error" >$cmdfile 2>&1
+	fi
+	exit 1
+}
+
+vpn_list() {
+	if [ ! -z "$wireguard_process" ] ; then
+		cd $wireguardpath
+		/usr/bin/vpn --list >$cmdfile 2>&1
+	else
+		echo "$wireguard_error" >$cmdfile 2>&1
+	fi
+	exit 1
+}
+
+vpn_route() {
+	if [ ! -z "$wireguard_process" ] ; then
+		cd $wireguardpath
+		./vnt-cli /usr/bin/vpn --route >$cmdfile 2>&1
+	else
+		echo "$wireguard_error" >$cmdfile 2>&1
+	fi
+	exit 1
+}
+
+wireguard_status() {
+	if [ ! -z "$vpn_process" ] ; then
+		wireguardcpu="$(top -b -n1 | grep -E "$(pidof vpn)" 2>/dev/null| grep -v grep | awk '{for (i=1;i<=NF;i++) {if ($i ~ /vpn/) break; else cpu=i}} END {print $cpu}')"
+		echo -e "\t\t vpn 运行状态\n" >$cmdfile
+		[ ! -z "$vpncpu" ] && echo "CPU占用 ${vpncpu}% " >>$cmdfile 2>&1
+		vpnram="$(cat /proc/$(pidof vpn | awk '{print $NF}')/status|grep -w VmRSS|awk '{printf "%.2fMB\n", $2/1024}')"
+		[ ! -z "$vpnram" ] && echo "内存占用 ${vpnram}" >>$cmdfile 2>&1
+		vnttime=$(cat /tmp/vntcli_time) 
+		if [ -n "$vnttime" ] ; then
+			time=$(( `date +%s`-vnttime))
+			day=$((time/86400))
+			[ "$day" = "0" ] && day=''|| day=" $day天"
+			time=`date -u -d @${time} +%H小时%M分%S秒`
+		fi
+		[ ! -z "$time" ] && echo "已运行 $time" >>$cmdfile 2>&1
+		cmdtart=$(cat /tmp/vpn.CMD)
+		[ ! -z "$cmdtart" ] && echo "启动参数  $cmdtart" >>$cmdfile 2>&1
+		
+	else
+		echo "$vpn_error" >$cmdfile
+	fi
+	exit 1
+}
+
+case $1 in
+start)
+	start_wireguard &
+	;;
+stop)
+	stop_wireguard
+	;;
+restart)
+	stop_wireguard
+	start_wireguard &
+	;;
+update)
+	update_wireguard &
+	;;
+vpninfo)
+	vpn_info
+	;;
+vpnall)
+	vpn_all
+	;;
+vpnlist)
+	vpn_list
+	;;
+vpnroute)
+	vnt_route
+	;;
+vpnstatus)
+	vpn_status
+	;;
+*)
+	echo "check"
+	#exit 0
+	;;
+esac
